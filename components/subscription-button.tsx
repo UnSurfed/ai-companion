5b1e1f71cc5f4a9d2c4b8567801315b5b9cc6639
UnSurfed <adlcruz.0923@gmail.com>
2023-08-13 22:19:22 -0700

    first commit

---
+++ components/subscription-button.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Sparkles } from "../node_modules/lucide-react";
+
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@/components/ui/use-toast";
+
+
+interface SubscritionButtonProps {
+    isPro: boolean;
+}
+
+export const SubscriptionButton = ({
+    isPro = false
+}: SubscritionButtonProps) => {
+    const [loading, setLoading]  = useState(false);
+    const { toast }  = useToast();
+
+    const onClick = async () => {
+        try {
+            setLoading(true);
+
+            const response = await axios.get("/api/stripe");
+            window.location .href = response.data.url;
+        } catch (error) {
+            toast({
+                variant: "destructive",
+                description: "Something went wrong."
+            })
+        }   finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <Button onClick={onClick} disabled={loading} size="sm" variant={isPro ? "default" : "premium"}>    
+        {isPro ? "Manage Subscription" : "Upgrade"}
+        {!isPro && <Sparkles className="h-4 w-4 ml-2 fill-white"/>}
+    </Button>
+    )
+    
+}
